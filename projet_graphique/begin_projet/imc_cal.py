57d9d0f8112cedef77ee817ef61bdae0a999ef14
Oriblast <kkenneth967@gmail.com>
2024-12-22 21:06:56 +0000

    ok

---
+++ projet_graphique/begin_projet/imc_cal.py
@@ -0,0 +1,18 @@
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App 
+
+class MainApp(App):
+    pass
+
+class HomeInterface(BoxLayout):
+    pass
+
+class LoginPage(BoxLayout):
+    pass
+
+class SignupPage(BoxLayout):
+    pass
+
+app = MainApp()
+app.run()
