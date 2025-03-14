747a1c7ea79257d6fb9c8c10804601a8ea53bc57
Oriblast <kkenneth967@gmail.com>
2025-03-14 14:50:01 +0000

    add : regression logistique optimisation mini batch  maj 1.2

--- dsir/algo_optimisation/logreg_train.py
+++ dsir/algo_optimisation/logreg_train.py
@@ -21,24 +21,56 @@ def make_data1(arow1, arow2, set_):
         writer.writerow(arow1)
         writer.writerow(arow2)
 
-def sigmoïde(feature, w, b):
+def maj_b(b2, feature, y2, set_name, l):
+    b = 0
+    w = [0] * len(y2)
+    """il s'agit de la dérivé de j sur w et b"""
+    for i in range(len(y2)):
+        if (feature["set"][l] == set_name):
+            y = 1
+        else:
+            y = 0
+        b += y2[i] - y
+        l += 1
+    b = b2.iloc[0, 0] - b2.iloc[0, 1] * ((1/len(y2)) * b)
+    make_data2(["b", "a"], [b, 0.03], set_name)
+
+def maj_w(w2, feature, y2, set_name, a, l):
+    w = w2.copy()
+    """il s'agit de la dérivé de j sur w et b"""
+    for j in feature.columns:
+        if j != "set":
+            w.loc[0,j] = 0
+            for i in range(len(y2)):
+                if (feature["set"][l] == set_name):
+                    y = 1
+                else:
+                    y = 0
+                w.loc[0, j] += (y2[i] - y) * feature[j][l]
+                l += 1
+    for i in w2.columns:
+        w2.loc[0, i] = w2.loc[0, i] - a * ((1/len(y2)) * w[i][0])
+    make_data1(columnsName_ls(w2), columns_ls(w2), set_name)
+
+def sigmoïde(feature, w, b, set_name, b2):
     """sigmoide fonction to determine probability y"""
     count = 0
     z = [0.0] * len(feature)
     y2 = [0] * len(feature)
+    y3 = [0] * mini_batch + 1
     for i in feature.columns:
         if i != s1:
             for j in range(len(feature)):
                 z[j] = feature[i][j] * w[i][0] + b
-
-                if mini == batch or j + 1 == len(feature):
-                    maj_b(data_b, feature, y2, set_name)
-                    maj_w(data_w, feature, y2, set_name, data_b["a"][0])
-                
-    for i in range(len(z)):
-        z[i] = (z[i] + b)
-    for i in range(len(y2)):
-        y2[i] = 1 / (1 + math.exp(-z[i]))
+                y2[j] = 1 / (1 + math.exp(-z[j]))
+                y3[mini] = y2[j]
+                mini += 1
+                if mini == mini_batch or j + 1 == len(feature):
+                    y3[mini+1] = 555 
+                    maj_b(b2, feature, y3[:mini], set_name, j - mini)
+                    maj_w(w, feature, y3[:mini], set_name, b2["a"][0], j - mini)
+                    mini = 0
+            
     return y2
 
 def columnsName_ls(feature):
@@ -74,35 +106,6 @@ def log_loss(feature, set_name, y2):
     print(ll)
     return ll
 
-def maj_b(b2, feature, y2, set_name):
-    b = 0
-    w = [0] * len(feature)
-    """il s'agit de la dérivé de j sur w et b"""
-    for i in range(len(feature)):
-        if (feature["set"][i] == set_name):
-            y = 1
-        else:
-            y = 0
-        b += y2[i] - y
-    b = b2.iloc[0, 0] - b2.iloc[0, 1] * ((1/len(feature)) * b)
-    make_data2(["b", "a"], [b, 0.03], set_name)
-
-def maj_w(w2, feature, y2, set_name, a):
-    w = w2.copy()
-    """il s'agit de la dérivé de j sur w et b"""
-    for j in feature.columns:
-        if j != "set":
-            w.loc[0,j] = 0
-            for i in range(len(feature)):
-                if (feature["set"][i] == set_name):
-                    y = 1
-                else:
-                    y = 0
-                w.loc[0, j] += (y2[i] - y) * feature[j][i]
-    for i in w2.columns:
-        w2.loc[0, i] = w2.loc[0, i] - a * ((1/len(feature)) * w[i][0])
-    make_data1(columnsName_ls(w2), columns_ls(w2), set_name)
-
 def rLogistic_train(feature, set_name):
     """entrainement donné """
     if not os.path.exists(set_name + "_biais.csv") or not os.path.exists(set_name + "_featureW.csv"):
@@ -113,7 +116,7 @@ def rLogistic_train(feature, set_name):
         make_data1(ls, init_data, set_name)
     data_b = pd.read_csv(set_name + "_biais.csv")
     data_w = pd.read_csv(set_name + "_featureW.csv")
-    y2 = sigmoïde(feature, data_w, data_b.iloc[0, 0])
+    y2 = y2 = sigmoïde(feature, data_w, data_b.iloc[0, 0], set_name, data_b)
     j = log_loss(feature, set_name, y2)
     if j > 0.01:
         return j
