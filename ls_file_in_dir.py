793efa8a7a5a2bf4751b96961611ab05258a6f58
kenneth songbe <103575756+Oriblast@users.noreply.github.com>
2025-09-05 01:35:25 +0200

    Update ls_file_in_dir.py

--- ls_file_in_dir.py
+++ ls_file_in_dir.py
@@ -1,5 +1,7 @@
 import os 
 import glob
+import matplotlib.pyplot as plt
+
 def ls_of_file(dir):
         ls_dir = []
         ls_file = {}
@@ -9,3 +11,32 @@ def ls_of_file(dir):
                 ls_file[i] = glob.glob((os.path.join(i, '*'))
         ls_file = glob.glob(modele)
         return ls_file
+
+def main():
+        if not sys.argv:
+                return -1
+        ls_element = ls_of_file(sys.argv[0])
+        x = []
+        y = []
+        for i in ls_element:
+                x.append(i.split('/')[-1]))
+                y.append(len(ls_element[i])) 
+
+        plt.bar(x, y)
+        plt.title("distribution")
+        plt.xlabel("type_img")
+        plt.ylabel("img_nb")
+        plt.xticks(rotation=45, ha="rigth") #ratation du texte pour  la lisebilité 
+        plt.tight_layout() #gestion auto de la mise en page 
+        plt.savefig("distribution.png")
+        plt.show()
+
+        plt.figure(figsize=(10, 10))
+        plt.pie(y, labels=x, autopct='%1.1f%%') # autopct montre les pourcentages
+        plt.title(f"Répartition des images par maladie\n({base_path.name})")
+        plt.tight_layout()
+        plt.savefig(f'diagramme_circulaire_{base_path.name}.png')
+        plt.show()
+        
+        
+                
